b6c55fe701bcb5430e3cffb5218ae208f74e24d3
Stephdotnet <stephane@molano.fr>
2023-01-16 06:38:17 +0000

    docker-compose setup & db import

---
+++ .devcontainer/database-structure.sql
@@ -0,0 +1,18 @@
+-- Adminer 4.8.1 MySQL 8.0.31 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `todos`;
+CREATE TABLE `todos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` text NOT NULL,
+  `completed` tinyint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 2023-01-15 17:27:03
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 - run `docker-compose -f .devcontainer/docker-compose.yml up`
 - composer install
+- import database structure `cat .devcontainer/database-structure.sql | docker-compose -f .devcontainer/docker-compose.yml exec -T mysql mysql -u user -ppassword main`
+
