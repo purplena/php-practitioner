cbe25a8f711f83973945d92d1357407955e458c9
= <=>
2023-03-04 12:03:56 +0100

    update sql structure in database-structure.sql

--- .devcontainer/database-structure.sql
+++ .devcontainer/database-structure.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 8.0.31 dump
+-- Adminer 4.8.1 MySQL 8.0.30 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -11,8 +11,21 @@ DROP TABLE IF EXISTS `todos`;
 CREATE TABLE `todos` (
   `id` int NOT NULL AUTO_INCREMENT,
   `description` text NOT NULL,
-  `completed` tinyint NOT NULL DEFAULT 0,
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `completed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- 2023-01-15 17:27:03
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(255) NOT NULL DEFAULT 'NULL',
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- 2023-03-04 10:52:24
